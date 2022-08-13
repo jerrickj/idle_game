c981469233a1a841cec8adf7dfb1f15dde50d1f0
jerrickj <jerrickj96@gmail.com>
2022-08-13 00:05:16 -0400

    Update idlegame_main.py

--- idlegame_main.py
+++ idlegame_main.py
@@ -1,32 +1,18 @@
-# sourcery skip: merge-nested-ifs
 import pygame
 import pygame_gui
 
 
 class IdleGame:
     def __init__(self) -> None:
-
+        # Define button size and position variables
         WINDOW_WIDTH = 800
         WINDOW_HEIGHT = 600
 
-        pygame.init()
-
-        pygame.display.set_caption("Idle Clicker")
-
-        self.window_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
-        self.background = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
-        self.background.fill(pygame.Color("#111111"))
-
-        self.manager = pygame_gui.UIManager((WINDOW_WIDTH, WINDOW_HEIGHT))
-
-        # Define button size and position variables
-
         OUTER_BORDER_WIDTH = 10
-        BUTTON_PADDING = 20
+        BUTTON_PADDING = 19
 
-        BUTTON_WIDTH = WINDOW_WIDTH // 6 - OUTER_BORDER_WIDTH * 2
-        BUTTON_HEIGHT = WINDOW_HEIGHT // 12 - OUTER_BORDER_WIDTH * 2
+        BUTTON_WIDTH = WINDOW_WIDTH // 7
+        BUTTON_HEIGHT = WINDOW_HEIGHT // 12
 
         BUTTON_ROW_1_Y_POS = WINDOW_HEIGHT - BUTTON_HEIGHT - OUTER_BORDER_WIDTH
         BUTTON_ROW_2_Y_POS = BUTTON_ROW_1_Y_POS - BUTTON_HEIGHT - BUTTON_PADDING
@@ -34,16 +20,71 @@ class IdleGame:
         PLAYER_MONEY_LABEL_WIDTH = 200
         PLAYER_MONEY_LABEL_HEIGHT = 75
 
-        # Define Main Stored Game Variables; these are the variables that are saved to the game save file
+        PLAYER_MONEY_LABEL_X_POS = (
+            (WINDOW_WIDTH // 2)
+            - (PLAYER_MONEY_LABEL_WIDTH // 2)
+            - (OUTER_BORDER_WIDTH // 2)
+        )
+        PLAYER_MONEY_LABEL_Y_POS = OUTER_BORDER_WIDTH
 
+        FUNCTION_BUTTON_WIDTH = 200
+        FUNCTION_BUTTON_HEIGHT = 75
+
+        # WINDOW_WIDTH // 2 - (PLAYER_MONEY_LABEL_WIDTH // 2), OUTER_BORDER_WIDTH
+
+        # Define Main Stored Game Variables; these are the variables that are saved to the game save file
         self.PLAYER_STARTING_MONEY = 50
         self.player_money = self.PLAYER_STARTING_MONEY
         self.player_level = 1
-        self.level_up_cost = 50000
+        self.level_up_cost = 5000  # Change this to increase the cost of leveling up
 
-        self.constant_money_increase = 0.25  # This is the amount of money that is added to the player's money every tick no matter what
+        # Set up default and starting values for player money
+        self.constant_money_increase = 0.1  # This is the default amount of money that is added to the player's money every tick no matter what
+        self.total_money_per_tick = (
+            self.constant_money_increase
+        )  # This is the total amount of money that is added to the player's money every tick
 
-        self.total_money_per_tick = self.constant_money_increase
+        # Set auto buy variables
+        self.auto_buy = False
+        self.AUTO_BUY_PRICE = 2500
+
+        # Prices to purchase one of each item
+        self.ITEM_1_PRICE, self.ITEM_1_VALUE, self.ITEM_1_QUANTITY = 20, 0.01, 0
+        self.ITEM_2_PRICE, self.ITEM_2_VALUE, self.ITEM_2_QUANTITY = 50, 0.10, 0
+        self.ITEM_3_PRICE, self.ITEM_3_VALUE, self.ITEM_3_QUANTITY = 100, 0.30, 0
+        self.ITEM_4_PRICE, self.ITEM_4_VALUE, self.ITEM_4_QUANTITY = 200, 0.75, 0
+        self.ITEM_5_PRICE, self.ITEM_5_VALUE, self.ITEM_5_QUANTITY = 400, 0.20, 0
+        self.ITEM_6_PRICE, self.ITEM_6_VALUE, self.ITEM_6_QUANTITY = 800, 0.50, 0
+        self.ITEM_7_PRICE, self.ITEM_7_VALUE, self.ITEM_7_QUANTITY = 1600, 1.20, 0
+        self.ITEM_8_PRICE, self.ITEM_8_VALUE, self.ITEM_8_QUANTITY = 3200, 2.50, 0
+        self.ITEM_9_PRICE, self.ITEM_9_VALUE, self.ITEM_9_QUANTITY = 6400, 6.00, 0
+        self.ITEM_10_PRICE, self.ITEM_10_VALUE, self.ITEM_10_QUANTITY = 12800, 15.00, 0
+        self.ITEM_11_PRICE, self.ITEM_11_VALUE, self.ITEM_11_QUANTITY = 25600, 35.00, 0
+        self.ITEM_12_PRICE, self.ITEM_12_VALUE, self.ITEM_12_QUANTITY = 51200, 75.00, 0
+
+        self.AUTO_BUY_FUNCS = [
+            (self.buy_item_1_button_pressed, self.ITEM_1_VALUE),
+            (self.buy_item_2_button_pressed, self.ITEM_2_VALUE),
+            (self.buy_item_3_button_pressed, self.ITEM_3_VALUE),
+            (self.buy_item_4_button_pressed, self.ITEM_4_VALUE),
+            (self.buy_item_5_button_pressed, self.ITEM_5_VALUE),
+            (self.buy_item_6_button_pressed, self.ITEM_6_VALUE),
+            (self.buy_item_7_button_pressed, self.ITEM_7_VALUE),
+            (self.buy_item_8_button_pressed, self.ITEM_8_VALUE),
+            (self.buy_item_9_button_pressed, self.ITEM_9_VALUE),
+            (self.buy_item_10_button_pressed, self.ITEM_10_VALUE),
+            (self.buy_item_11_button_pressed, self.ITEM_11_VALUE),
+            (self.buy_item_12_button_pressed, self.ITEM_12_VALUE),
+        ]
+
+        pygame.init()
+        pygame.display.set_caption("Idle Clicker")
+
+        self.window_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.background = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.background.fill(pygame.Color("#111111"))
+
+        self.manager = pygame_gui.UIManager((WINDOW_WIDTH, WINDOW_HEIGHT))
 
         self.player_level_label = pygame_gui.elements.UILabel(
             relative_rect=pygame.Rect(
@@ -53,16 +94,43 @@ class IdleGame:
                 ),
                 (PLAYER_MONEY_LABEL_WIDTH, PLAYER_MONEY_LABEL_HEIGHT),
             ),
-            text=f"Multiplier: {self.player_level}",
+            text=f"Player Level: {self.player_level}",
             manager=self.manager,
         )
 
-        self.player_money_label = pygame_gui.elements.UILabel(
+        self.player_level_up_button = pygame_gui.elements.UIButton(
             relative_rect=pygame.Rect(
                 (
-                    WINDOW_WIDTH // 2 - (PLAYER_MONEY_LABEL_WIDTH // 2),
                     OUTER_BORDER_WIDTH,
+                    OUTER_BORDER_WIDTH + PLAYER_MONEY_LABEL_HEIGHT + BUTTON_PADDING,
                 ),
+                (FUNCTION_BUTTON_WIDTH, FUNCTION_BUTTON_HEIGHT),
+            ),
+            text=f"Level Up: -${self.level_up_cost}",
+            manager=self.manager,
+            tool_tip_text="This button will level up your player and increase your money per tick by 150%",
+        )
+
+        self.auto_buy_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect(
+                (
+                    OUTER_BORDER_WIDTH,
+                    OUTER_BORDER_WIDTH
+                    + (PLAYER_MONEY_LABEL_HEIGHT * 2)
+                    + (BUTTON_PADDING * 2),
+                ),
+                (FUNCTION_BUTTON_WIDTH, FUNCTION_BUTTON_HEIGHT),
+            ),
+            text="Auto Buy",
+            manager=self.manager,
+            tool_tip_text="This button will buy affordable items automatically",
+            visible=False,
+        )
+        self.auto_buy_button.visible = False
+
+        self.player_money_label = pygame_gui.elements.UILabel(
+            relative_rect=pygame.Rect(
+                (PLAYER_MONEY_LABEL_X_POS, PLAYER_MONEY_LABEL_Y_POS),
                 (PLAYER_MONEY_LABEL_WIDTH, PLAYER_MONEY_LABEL_HEIGHT),
             ),
             text=f"Player Money: {self.player_money}",
@@ -83,67 +151,13 @@ class IdleGame:
             manager=self.manager,
         )
 
-        self.player_level_up_button = pygame_gui.elements.UIButton(
-            relative_rect=pygame.Rect(
-                (
-                    WINDOW_WIDTH // 2 - (PLAYER_MONEY_LABEL_WIDTH // 2),
-                    OUTER_BORDER_WIDTH + PLAYER_MONEY_LABEL_HEIGHT + BUTTON_PADDING,
-                ),
-                (200, 100),
-            ),
-            text=f"Level Up: -${self.level_up_cost}",
-            manager=self.manager,
-        )
-
-        # Prices to purchase one of each item
-        self.ITEM_1_PRICE = 20
-        self.ITEM_2_PRICE = 100
-        self.ITEM_3_PRICE = 500
-        self.ITEM_4_PRICE = 2000
-        self.ITEM_5_PRICE = 5000
-        self.ITEM_6_PRICE = 12000
-        self.ITEM_7_PRICE = 20000
-        self.ITEM_8_PRICE = 50000
-        self.ITEM_9_PRICE = 100000
-        self.ITEM_10_PRICE = 200000
-        self.ITEM_11_PRICE = 500000
-        self.ITEM_12_PRICE = 1000000
-
-        # Amount of each item that the player has
-        self.ITEM_1_QUANTITY = 0
-        self.ITEM_2_QUANTITY = 0
-        self.ITEM_3_QUANTITY = 0
-        self.ITEM_4_QUANTITY = 0
-        self.ITEM_5_QUANTITY = 0
-        self.ITEM_6_QUANTITY = 0
-        self.ITEM_7_QUANTITY = 0
-        self.ITEM_8_QUANTITY = 0
-        self.ITEM_9_QUANTITY = 0
-        self.ITEM_10_QUANTITY = 0
-        self.ITEM_11_QUANTITY = 0
-        self.ITEM_12_QUANTITY = 0
-
-        # Item Values Added to Player's Money (Multiplied by Quantity)
-        self.ITEM_1_VALUE = 0.25
-        self.ITEM_2_VALUE = 1
-        self.ITEM_3_VALUE = 2
-        self.ITEM_4_VALUE = 4
-        self.ITEM_5_VALUE = 6
-        self.ITEM_6_VALUE = 12
-        self.ITEM_7_VALUE = 25
-        self.ITEM_8_VALUE = 100
-        self.ITEM_9_VALUE = 250
-        self.ITEM_10_VALUE = 500
-        self.ITEM_11_VALUE = 750
-        self.ITEM_12_VALUE = 1000
-
         # Create a UI element for each item in the shop
         self.buy_item_1_button = pygame_gui.elements.UIButton(
             relative_rect=pygame.Rect(
                 (OUTER_BORDER_WIDTH, BUTTON_ROW_1_Y_POS),
                 (BUTTON_WIDTH, BUTTON_HEIGHT),
             ),
-            text=f"${self.ITEM_1_PRICE}: +{self.ITEM_1_VALUE}",
+            text=f"${self.shorten_number(self.ITEM_1_PRICE)}: +{self.ITEM_1_VALUE}",
             manager=self.manager,
         )
 
@@ -155,7 +169,7 @@ class IdleGame:
                 ),
                 (BUTTON_WIDTH, BUTTON_HEIGHT),
             ),
-            text=f"${self.ITEM_2_PRICE}: +{self.ITEM_2_VALUE}",
+            text=f"${self.shorten_number(self.ITEM_2_PRICE)}: +{self.ITEM_2_VALUE}",
             manager=self.manager,
         )
 
@@ -167,7 +181,7 @@ class IdleGame:
                 ),
                 (BUTTON_WIDTH, BUTTON_HEIGHT),
             ),
-            text=f"${self.ITEM_3_PRICE}: +{self.ITEM_3_VALUE}",
+            text=f"${self.shorten_number(self.ITEM_3_PRICE)}: +{self.ITEM_3_VALUE}",
             manager=self.manager,
         )
 
@@ -179,7 +193,7 @@ class IdleGame:
                 ),
                 (BUTTON_WIDTH, BUTTON_HEIGHT),
             ),
-            text=f"${self.ITEM_4_PRICE}: +{self.ITEM_4_VALUE}",
+            text=f"${self.shorten_number(self.ITEM_4_PRICE)}: +{self.ITEM_4_VALUE}",
             manager=self.manager,
         )
 
@@ -191,7 +205,7 @@ class IdleGame:
                 ),
                 (BUTTON_WIDTH, BUTTON_HEIGHT),
             ),
-            text=f"${self.ITEM_5_PRICE}: +{self.ITEM_5_VALUE}",
+            text=f"${self.shorten_number(self.ITEM_5_PRICE)}: +{self.ITEM_5_VALUE}",
             manager=self.manager,
         )
 
@@ -203,16 +217,16 @@ class IdleGame:
                 ),
                 (BUTTON_WIDTH, BUTTON_HEIGHT),
             ),
-            text=f"${self.ITEM_6_PRICE}: +{self.ITEM_6_VALUE}",
+            text=f"${self.shorten_number(self.ITEM_6_PRICE)}: +{self.ITEM_6_VALUE}",
             manager=self.manager,
         )
-
+        # Buttons 7 - 12 are only visible if the player has leveled enough to buy them
         self.buy_item_7_button = pygame_gui.elements.UIButton(
             relative_rect=pygame.Rect(
                 (OUTER_BORDER_WIDTH, BUTTON_ROW_2_Y_POS),
                 (BUTTON_WIDTH, BUTTON_HEIGHT),
             ),
-            text=f"${self.ITEM_7_PRICE}: +{self.ITEM_7_VALUE}",
+            text=f"${self.shorten_number(self.ITEM_7_PRICE)}: +{self.ITEM_7_VALUE}",
             manager=self.manager,
             visible=False,
         )
@@ -225,7 +239,7 @@ class IdleGame:
                 ),
                 (BUTTON_WIDTH, BUTTON_HEIGHT),
             ),
-            text=f"${self.ITEM_8_PRICE}: +{self.ITEM_8_VALUE}",
+            text=f"${self.shorten_number(self.ITEM_8_PRICE)}: +{self.ITEM_8_VALUE}",
             manager=self.manager,
             visible=False,
         )
@@ -238,7 +252,7 @@ class IdleGame:
                 ),
                 (BUTTON_WIDTH, BUTTON_HEIGHT),
             ),
-            text=f"${self.ITEM_9_PRICE}: +{self.ITEM_9_VALUE}",
+            text=f"${self.shorten_number(self.ITEM_9_PRICE)}: +{self.ITEM_9_VALUE}",
             manager=self.manager,
             visible=False,
         )
@@ -251,7 +265,7 @@ class IdleGame:
                 ),
                 (BUTTON_WIDTH, BUTTON_HEIGHT),
             ),
-            text=f"${self.ITEM_10_PRICE}: +{self.ITEM_10_VALUE}",
+            text=f"${self.shorten_number(self.ITEM_10_PRICE)}: +{self.ITEM_10_VALUE}",
             manager=self.manager,
             visible=False,
         )
@@ -264,7 +278,7 @@ class IdleGame:
                 ),
                 (BUTTON_WIDTH, BUTTON_HEIGHT),
             ),
-            text=f"${self.ITEM_11_PRICE}: +{self.ITEM_11_VALUE}",
+            text=f"${self.shorten_number(self.ITEM_11_PRICE)}: +{self.ITEM_11_VALUE}",
             manager=self.manager,
             visible=False,
         )
@@ -277,14 +291,27 @@ class IdleGame:
                 ),
                 (BUTTON_WIDTH, BUTTON_HEIGHT),
             ),
-            text=f"${self.ITEM_12_PRICE}: +{self.ITEM_12_VALUE}",
+            text=f"${self.shorten_number(self.ITEM_12_PRICE)}: +{self.ITEM_12_VALUE}",
             manager=self.manager,
             visible=False,
         )
 
-        # Buttons 7 - 12 are only visible if the player has leveled enough to buy them
-
         self.main()
+
+    # Shorten input to 2 decimal places
+    def truncate(self, input, decimals=2):
+        multiplier = 10**decimals
+        return int(input * multiplier) / multiplier
+
+    def shorten_number(self, number):
+        if number > 999:
+            return f"{number / 1000}K"
+        if number > 999999:
+            return f"{number / 1000000}M"
+        if number > 999999999:
+            return f"{number / 1000000000}B"
+        else:
+            return number
 
     def buy_item_1_button_pressed(self):
         if self.player_money >= self.ITEM_1_PRICE:
@@ -360,9 +387,11 @@ class IdleGame:
 
     def player_level_up_button_pressed(self):
         self.player_level += 1
-        self.player_money -= self.level_up_cost
-        self.total_money_per_tick * 1.5
-        self.level_up_cost *= 2.5
+        self.player_money -= (
+            self.level_up_cost
+        )  # Subtract cost of level up from the player's money
+        self.total_money_per_tick *= 1.5  # increase money per tick by 50%
+        self.level_up_cost *= 2  # increase level up cost by 200%
 
         self.player_level_label.set_text(f"Player Level: {self.player_level}")
         self.money_increase_label.set_text(f"+{self.total_money_per_tick} per tick)")
@@ -378,7 +407,17 @@ class IdleGame:
             self.buy_item_11_button.visible = True
             self.buy_item_12_button.visible = True
 
-    def check_affordable(self):
+    def auto_buy_button_pressed(self):
+        self.auto_buy = not self.auto_buy
+
+        if self.auto_buy:
+            self.auto_buy_button.set_text("Auto Buy: ON")
+
+        if not self.auto_buy:
+            self.auto_buy_button.set_text("Auto Buy: OFF")
+            self.auto_buy_button.colours = (190, 190, 190)
+
+    def check_affordable_items(self):
         if self.player_money >= self.ITEM_1_PRICE:
             self.buy_item_1_button.enable()
         else:
@@ -441,24 +480,20 @@ class IdleGame:
 
     def check_player_level(self):
         if self.player_money >= self.level_up_cost:
+            self.player_level_up_button.visible = True
             self.player_level_up_button.enable()
         else:
+            # self.player_level_up_button.set_text(f"Level Up: -${self.level_up_cost}")
             self.player_level_up_button.disable()
+            self.player_level_up_button.visible = False
 
-        if self.player_level >= 5:
-            self.buy_item_4_button.enable()
-            self.buy_item_5_button.enable()
-            self.buy_item_6_button.enable()
-        if self.player_level >= 10:
-            self.buy_item_7_button.enable()
-            self.buy_item_8_button.enable()
-            self.buy_item_9_button.enable()
-        if self.player_level >= 15:
-            self.buy_item_10_button.enable()
-            self.buy_item_11_button.enable()
-            self.buy_item_12_button.enable()
+    def check_auto_buy_req(self):
+        if self.player_level >= 2:
+            self.auto_buy_button.visible = True
+        if self.player_money >= self.AUTO_BUY_PRICE:
+            self.auto_buy_button.enable()
 
-    def add_money(self):
+    def add_money(self):  # add money per tick
         self.player_money += self.constant_money_increase
         self.player_money += self.ITEM_1_QUANTITY * self.ITEM_1_VALUE
         self.player_money += self.ITEM_2_QUANTITY * self.ITEM_2_VALUE
@@ -473,12 +508,16 @@ class IdleGame:
         self.player_money += self.ITEM_11_QUANTITY * self.ITEM_11_VALUE
         self.player_money += self.ITEM_12_QUANTITY * self.ITEM_12_VALUE
 
-        self.player_money * self.player_level
+        self.player_money = self.truncate(self.player_money)
 
-        self.player_money_label.set_text(f"Player Money: {self.player_money}")
-        self.money_increase_label.set_text(f"(+{self.total_money_per_tick} per tick)")
+        self.player_money_label.set_text(
+            f"Player Money: {self.shorten_number(self.player_money)}"
+        )
+        self.money_increase_label.set_text(
+            f"(+{self.truncate(self.total_money_per_tick)} per tick)"
+        )
 
-    def check_events(self, event):
+    def check_button_events(self, event):
         if event.ui_element == self.buy_item_1_button:
             self.buy_item_1_button_pressed()
         if event.ui_element == self.buy_item_2_button:
@@ -505,10 +544,20 @@ class IdleGame:
             self.buy_item_12_button_pressed()
         if event.ui_element == self.player_level_up_button:
             self.player_level_up_button_pressed()
+        if event.ui_element == self.auto_buy_button:
+            self.auto_buy_button_pressed()
+
+    class Menu:
+        def __init__(self, screen):
+            self.screen = screen
+            self.screen_rect = screen.get_rect()
+            self.menu_items = []
+            self.font = pygame.font.SysFont(None, 48)
+            self.prep_menu()
 
     def main(self):
 
-        fps = 10
+        self.fps = 10
 
         clock = pygame.time.Clock()
 
@@ -516,11 +565,12 @@ class IdleGame:
 
         while is_running:
 
-            time_delta = clock.tick(fps) / 1000.0
+            time_delta = clock.tick(self.fps) / 1000.0
 
-            self.check_affordable()
-            self.check_player_level()
-            self.add_money()
+            if self.auto_buy:
+                for func, item in self.AUTO_BUY_FUNCS:
+                    if self.player_money >= item:
+                        func()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -528,9 +578,24 @@ class IdleGame:
 
                 if event.type == pygame_gui.UI_BUTTON_PRESSED:
 
-                    self.check_events(event)
+                    self.check_button_events(event)
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_m:
+                        self.Menu()
+
+                    if event.key == pygame.K_q:
+                        is_running = False
+
+                    if event.key == pygame.K_p:
+                        self.pause_game()
 
                 self.manager.process_events(event)
+
+            self.check_affordable_items()
+            self.check_player_level()
+            self.check_auto_buy_req()
+            self.add_money()
 
             self.manager.update(time_delta)
 
